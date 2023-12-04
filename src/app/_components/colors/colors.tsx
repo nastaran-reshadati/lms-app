a68e0f8a1a29cde6ecf806e9fb107d5dd55c6356
nastaran reshadati <96713994+nastaran-reshadati@users.noreply.github.com>
2023-12-04 17:23:46 +0330

    tailwind theme & color

---
+++ src/app/_components/colors/colors.tsx
@@ -0,0 +1,10 @@
+// import { tailwindColors } from "../../../../tailwind.config";
+
+// export const colors: React.FC = () => (
+//   <div className="flex flex-wrap justify-center">
+//     {Object.entries(tailwindColors).map([name, color]) => (
+//         <ColorBox key={name} name = {name} color={color}>
+//         </ColorBox>
+//     )}
+//   </div>
+// );
