73f60cf9d02a18dfffac6193fa08b002ae1c0883
nastaran reshadati <96713994+nastaran-reshadati@users.noreply.github.com>
2023-12-06 17:29:44 +0330

    add google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import "./globals.css";
+import { Figtree } from "next/font/google";
 
+const figTree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-figtree",
+});
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl">
+    <html lang="en" dir="rtl" className={`${figTree.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-400 flex justify-center items-center">
           Header
