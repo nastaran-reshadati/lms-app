bc7c7740e103f729e0e0785ddf5d8ef1e1e7313d
nastaran <nastaran.reshadati1376@gmail.com>
2025-06-04 19:22:30 +0330

    add footer component

--- src/app/_components/footer/footer.tsx
+++ src/app/_components/footer/footer.tsx
@@ -1,12 +1,77 @@
+/** @format */
+
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex justify-between dark:bg-base-200">
-      <div>1111</div>
-      <div>1111</div>
-      <div>1111</div>
-    </footer>
+    <div className="bg-base-100 text-base-content">
+      <footer className="container flex flex-col lg:flex-row items-center gap-5 px-0 lg:px-12 xl:px-40 py-20">
+        <div className="text-center flex flex-col items-center lg:me-20">
+          <Image
+            src="/images/logo.svg"
+            width={180}
+            height={36}
+            alt="لوگو سایت"
+          />
+
+          <p className="opacity-50 mt-2">
+            پلتفرم آموزش برنامه‌نویسی
+            <br /> یادگیری مداوم - پیشرفت مستمر
+          </p>
+        </div>
+        <div className="flex flex-1 flex-col md:flex-row gap-5 md:gap-6 whitespace-nowrap">
+          <div className="grid flex-1 basis-36 gap-3 place-items-center md:place-items-start">
+            <Link href="/docs/install/" className="link link-hover">
+              آموزش ری‌اکت و نکست
+            </Link>
+            <Link href="/docs/customize/" className="link link-hover">
+              دوره‌های پیشرفته ری‌اکت
+            </Link>
+            <Link href="/docs/customize/" className="link link-hover">
+              مشاوره برنامه‌نویسی
+            </Link>
+          </div>
+
+          <div className="grid flex-1 basis-36 gap-3 place-items-center md:place-items-start">
+            <Link href="/docs/themes/" className="link link-hover">
+              ما در لینکدین
+            </Link>
+            <Link href="/docs/themes/" className="link link-hover">
+              کانال تلگرام ما
+            </Link>
+            <Link href="/docs/themes/" className="link link-hover">
+              صفحه یوتیوب
+            </Link>
+          </div>
+          <div className="grid flex-1 basis-36 gap-3 place-items-center md:place-items-start">
+            <Link href="/docs/themes/" className="link link-hover">
+              مطالب و مقالات آموزشی
+            </Link>
+            <Link href="/docs/themes/" className="link link-hover">
+              پرسش‌های متداول
+            </Link>
+            <Link href="/docs/themes/" className="link link-hover">
+              قوانین استفاده و حریم خصوصی
+            </Link>
+          </div>
+        </div>
+      </footer>
+
+      <div className="bg-base-200 text-left" lang="en" dir="ltr">
+        <div className="container py-10 flex justify-between items-center">
+          <div className="flex gap-5 items-center ">
+            <div className="flex flex-col">
+              <span className="text-base-content/50 ">Developed by:</span>
+              <span className="text-lg font-bold tracking-wide">
+                Nastaran Reshadati{" "}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
